07af09fc7ae45164888a027a8b0953c8b9b1190f
BruvLio <lionel.mbayu@gmail.com>
2023-05-20 14:20:44 -0500

    wip

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -1,7 +1,9 @@
 const db = require('../../data/db-config.js');
 
 function find() {
-  return db('users')
+  return db('users as u')
+    .join('roles as r', 'u.role_id', '=', 'r.role_id')
+    .select('u.user_id','u.username', 'r.role_name')
     
   /**
     You will need to join two tables.
