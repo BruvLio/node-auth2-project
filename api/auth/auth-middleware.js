e6eccdf83ed4c2f10e6315a204ff58391c65e24b
BruvLio <lionel.mbayu@gmail.com>
2023-05-22 11:09:13 -0500

    wip

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -3,7 +3,20 @@ const { JWT_SECRET } = require("../secrets"); // use this secret!
 
 const restricted = (req, res, next) => {
   const token = req.headers.authorization
-  
+  if(token) {
+    jwt.verify(token, JWT_SECRET, (err, decoded) => {
+      if(err) {
+        next({status: 401, message: "Token invalid"})
+      } else {
+        req.decodedJwt = decoded
+        console.log(decoded)
+        next()
+      }
+    })
+    
+  } else {
+    next({status: 401, message: "Token required"})
+  }
   /*
     If the user does not provide a token in the Authorization header:
     status 401
