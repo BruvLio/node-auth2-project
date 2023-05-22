794bf511e472a90457b9dd61a466a45f86ffd1b8
BruvLio <lionel.mbayu@gmail.com>
2023-05-22 12:29:52 -0500

    wip

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -47,6 +47,7 @@ const only = role_name => (req, res, next) => {
 
     Pull the decoded token from the req object, to avoid verifying it again!
   */
+next()
 }
 
 
@@ -65,10 +66,12 @@ const checkUsernameExists = async (req, res, next) => {
       "message": "Invalid credentials"
     }
   */
+ 
 }
 
 
 const validateRoleName = (req, res, next) => {
+
   /*
     If the role_name in the body is valid, set req.role_name to be the trimmed string and proceed.
 
@@ -87,6 +90,17 @@ const validateRoleName = (req, res, next) => {
       "message": "Role name can not be longer than 32 chars"
     }
   */
+ if (!req.body.role_name || !req.body.role_name.trim()) {
+    req.role_name = 'student'
+    next()
+ } else if(req.body.role_name.trim() === 'admin') {
+    next({status: 422, message: 'Role name can not be admin'})
+ } else if(req.body.role_name.trim().length > 32) {
+    next({status: 422, message: 'Role name can not be longer than 32 chars'})
+ } else {
+  req.role_name = req.body.role_name.trim()
+    next()
+ }
 }
 
 module.exports = {
